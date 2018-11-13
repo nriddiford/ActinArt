7272f37acb3349be07ba83a99c3c1a2e9cb7f8b5
nriddiford <nickriddiford@me.com>
2018-11-13 22:03:04 +0100

    Update for paralax and better .js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,47 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 });
 
-// Scroll to id from nav items
-$(".navbar a").click(function () {
-    $("body,html").animate({
-        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
-    }, 1000)
 
+// Generic scrolling animation
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var element = this.hash;
+            var nav_offset = $('.navbar').height() + 15
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(element).offset().top-nav_offset}, 1000, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                // THis was causing problems 13.11.18
+                // window.location.hash = element;
+            });
+        } // End if
+    });
 });
-
-// Header button -> scroll down
-$(".header button").click(function () {
-    $("body,html").animate({
-        scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 10
-    }, 1000)
-
-});
-
-// // Reveal text on scroll through
-// window.sr = ScrollReveal({
-//     reset: true,
-//     easing: 'ease-in'
-// });
-// sr.reveal('.reveal', { opacity: 0.1, duration:600 });
-// ScrollReveal().reveal('.reveal', { reset: true, easing: 'ease-in', opacity: 0.1, duration:600 });
 
 ScrollReveal().reveal('.reveal', {reset: true})
+
+// Scroll to id from nav items
+// $(".navbar a").click(function () {
+//     $("body,html").animate({
+//         scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 14
+//     }, 1000)
+// });
+//
+// // Header button -> scroll down
+// $(".header button").click(function () {
+//     $("body,html").animate({
+//         scrollTop: $("#" + $(this).data('value')).offset().top - $('.navbar').height() - 14
+//     }, 1000)
+//
+// });
 
